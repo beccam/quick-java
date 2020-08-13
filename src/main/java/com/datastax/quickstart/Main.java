564ae6542953f409066bb9187ab0e8f0152da496
beccam <beccamills86@gmail.com>
2020-08-13 15:46:48 +0000

    new

--- src/main/java/com/datastax/quickstart/Main.java
+++ src/main/java/com/datastax/quickstart/Main.java
@@ -4,7 +4,7 @@ import com.datastax.oss.driver.api.core.CqlSession;
 import com.datastax.oss.driver.api.core.cql.*;
 import java.net.InetSocketAddress;
 
-public class GettingStartedComplete {
+public class Main {
 
     public static void main(String[] args) {
 
